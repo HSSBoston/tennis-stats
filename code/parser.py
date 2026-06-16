88f769e92c4d4d4f4a720d621247523ca49f7126
hssboston <hanna.s.suzuki@gmail.com>
2026-06-16 11:30:12 -0400

    parser rev

--- code/parser.py
+++ code/parser.py
@@ -1,34 +1,38 @@
 from pathlib import Path
 import pandas as pd
+from constants import GAME_STATES
 
 class Parser:
     CURRENT_DIR = Path(__file__).parent
     MCP_DIR     = CURRENT_DIR / "data" / "tennis_MatchChartingProject"
     OUTPUT_DIR  = CURRENT_DIR / "output"
-    
-    pointsPath = []
-    matchesPath = ""
-    points = pd.DataFrame(index=[], columns=[])
-    matches = pd.DataFrame(index=[], columns=[])
-    
+        
     def __init__(self,
         tour: str = "w",
         players: list[str] | None = None,
-        pointsFiles: list[Path] | None = None
+        pointsFiles: list[str] | None = None
     ) -> None:
+
+        self.pointsPaths: list[Path]
+        self.matchesPath: Path
+        self.points = pd.DataFrame
+        self.matches = pd.DataFrame
 
         if tour not in {"w", "m"}:
             raise ValueError("tour must be 'w' or 'm'")
 
         if pointsFiles == None:
             pointsFiles = [f"charting-{tour}-points-2020s.csv"]
+        if len(pointsFiles) == 0:
+            raise ValueError("pointsFiles must contain at least one filename")
         matchesFile = f"charting-{tour}-matches.csv"
 
         self.verifyPaths(tour, pointsFiles, matchesFile)
         self.loadPoints()
         self.loadMatches()
+#         self.verifyPlayers()
 
-    def verifyPaths(self, tour: str, pointsFiles: list[Path], matchesFile: str) -> None:
+    def verifyPaths(self, tour: str, pointsFiles: list[str], matchesFile: str) -> None:
         self.pointsPaths = [self.MCP_DIR / f for f in pointsFiles]
         self.matchesPath = self.MCP_DIR / matchesFile
 
@@ -46,12 +50,12 @@ class Parser:
                 # errors="coerce": replacing invalid value with NaN, not raising an exception
                 # "Int64": converting NaN to Pandas' nullable integer (pd.NA)
 #         print(df.head())
-        self.points = df
+        self.points = df[df["Pts"].isin(GAME_STATES)]
+        print(self.points.head())
         
     def loadMatches(self) -> None:
         self.matches = pd.read_csv(self.matchesPath, dtype=str)
 
-        
 
 
 if __name__ == "__main__":
