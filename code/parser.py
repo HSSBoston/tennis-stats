0c6a7d1db86a528b7bc818c363e3455bd664e0bd
hssboston <hanna.s.suzuki@gmail.com>
2026-06-16 10:38:49 -0400

    parser rev

--- code/parser.py
+++ code/parser.py
@@ -2,51 +2,66 @@ from pathlib import Path
 from typing import Optional
 import pandas as pd
 
-CURRENT_DIR = Path(__file__).parent
-MCP_DIR = CURRENT_DIR / "data" / "tennis_MatchChartingProject"
-OUTPUT_DIR = CURRENT_DIR / "output"
+class Parser:
+    CURRENT_DIR = Path(__file__).parent
+    MCP_DIR     = CURRENT_DIR / "data" / "tennis_MatchChartingProject"
+    OUTPUT_DIR  = CURRENT_DIR / "output"
+    
+    pointsPath = []
+    matchesPath = ""
+    points = pd.DataFrame(index=[], columns=[])
+    matches = pd.DataFrame(index=[], columns=[])
+    
+    def __init__(self,
+        tour: str = "w",
+        players: list[str] | None = None,
+        pointsFiles: list[Path] | None = None
+    ) -> None:
 
+        if tour not in {"w", "m"}:
+            raise ValueError("tour must be 'w' or 'm'")
 
+        if pointsFiles == None:
+            pointsFiles = [f"charting-{tour}-points-2020s.csv"]
+        matchesFile = f"charting-{tour}-matches.csv"
 
-def loadPointsFiles(paths: Path) -> pd.DataFrame:
-    frames = [pd.read_csv(p, low_memory=False, dtype=str) for p in paths]
-    df = pd.concat(frames, ignore_index=True)
-    for col in ("Pt", "Svr", "PtWinner"):
-        df[col] = pd.to_numeric(df[col], errors="coerce").astype("Int64")
-    return df
+        self.verifyPaths(tour, pointsFiles, matchesFile)
+        self.loadPoints()
+        self.loadMatches()
 
-def run(tour: str = "w",
-        pointsFiles: Optional[list] = None,
-        players: Optional[list] = None) -> None:
+    def verifyPaths(self, tour: str, pointsFiles: list[Path], matchesFile: str) -> None:
+        self.pointsPaths = [self.MCP_DIR / f for f in pointsFiles]
+        self.matchesPath = self.MCP_DIR / matchesFile
 
-    if pointsFiles == None:
-        pointsFiles = [f"charting-{tour}-points-2020s.csv"]
+        requiredPaths = self.pointsPaths + [self.matchesPath]
+        for path in requiredPaths:
+            if not path.is_file():
+                raise FileNotFoundError(f"Path does not exist: {path}")
+        print(f"Input file paths verified: {[f for f in pointsFiles] + [matchesFile]}")
+
+    def loadPoints(self) -> None:
+        frames = [pd.read_csv(p, dtype=str) for p in self.pointsPaths]
+        df = pd.concat(frames, axis=0, ignore_index=True)
+        for col in ("Pt", "Svr", "PtWinner"):
+            df[col] = pd.to_numeric(df[col], errors="coerce").astype("Int64")
+                # errors="coerce": replacing invalid value with NaN, not raising an exception
+                # "Int64": converting NaN to Pandas' nullable integer (pd.NA)
+#         print(df.head())
+        self.points = df
         
-    if players == None:
-        if tour == "w":
-            players = ["Iga Swiatek", "Aryna Sabalenka", "Coco Gauff",
-             "Elena Rybakina", "Jessica Pegula", "Mirra Andreeva",
-             "Ons Jabeur", "Maria Sakkari", "Karolina Pliskova"]
-        elif tour == "m":
-            players = ["Novak Djokovic", "Carlos Alcaraz", "Jannik Sinner",
-             "Daniil Medvedev", "Stefanos Tsitsipas",
-             "Alexander Zverev", "Andrey Rublev", "Casper Ruud",
-             "Hubert Hurkacz", "Rafael Nadal"]
+    def loadMatches(self) -> None:
+        self.matches = pd.read_csv(self.matchesPath, dtype=str)
 
-    pointsPaths = [MCP_DIR / f for f in pointsFiles]
-    matchesPath = MCP_DIR / f"charting-{tour}-matches.csv"
-    requiredPaths = pointsPaths + [matchesPath]
-    
-    for path in requiredPaths:
-#         if path.is_file():
-#             print(path)
-        if not path.is_file():
-            raise FileNotFoundError(f"Path does not exist: {path}")
-    
-    pts = loadPointsFiles(pointsPaths)
         
 
 
 if __name__ == "__main__":
-    run("w")
-    
+    playersW = ["Iga Swiatek", "Aryna Sabalenka", "Coco Gauff",
+                     "Elena Rybakina", "Jessica Pegula", "Mirra Andreeva",
+                     "Ons Jabeur", "Maria Sakkari", "Karolina Pliskova"]
+    playersM = ["Novak Djokovic", "Carlos Alcaraz", "Jannik Sinner",
+                     "Daniil Medvedev", "Stefanos Tsitsipas",
+                     "Alexander Zverev", "Andrey Rublev", "Casper Ruud",
+                     "Hubert Hurkacz", "Rafael Nadal"]
+
+    parser = Parser("w", playersW) 
