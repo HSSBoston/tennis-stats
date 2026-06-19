89458b8ecd7da2540cea77cb2d1fbb178956d8cf
hssboston <hanna.s.suzuki@gmail.com>
2026-06-19 17:14:08 -0400

    Create multi-player-edge.py

---
+++ code/multi-player-edge.py
@@ -0,0 +1,8 @@
+
+playersW = ["Iga Swiatek", "Aryna Sabalenka", "Coco Gauff",
+                 "Elena Rybakina", "Jessica Pegula", "Mirra Andreeva",
+                 "Ons Jabeur", "Maria Sakkari", "Karolina Pliskova"]
+playersM = ["Novak Djokovic", "Carlos Alcaraz", "Jannik Sinner",
+                 "Daniil Medvedev", "Stefanos Tsitsipas",
+                 "Alexander Zverev", "Andrey Rublev", "Casper Ruud",
+                 "Hubert Hurkacz", "Rafael Nadal"]
