8ecba0ad6de45c0eddef4606d56d03c081b4de48
hssboston <hanna.s.suzuki@gmail.com>
2026-06-19 17:15:42 -0400

    Update multi-player-edge.py

--- code/multi-player-edge.py
+++ code/multi-player-edge.py
@@ -1,8 +1,13 @@
+import pandas as pd
 
 playersW = ["Iga Swiatek", "Aryna Sabalenka", "Coco Gauff",
                  "Elena Rybakina", "Jessica Pegula", "Mirra Andreeva",
                  "Ons Jabeur", "Maria Sakkari", "Karolina Pliskova"]
-playersM = ["Novak Djokovic", "Carlos Alcaraz", "Jannik Sinner",
-                 "Daniil Medvedev", "Stefanos Tsitsipas",
-                 "Alexander Zverev", "Andrey Rublev", "Casper Ruud",
-                 "Hubert Hurkacz", "Rafael Nadal"]
+
+# playersM = ["Novak Djokovic", "Carlos Alcaraz", "Jannik Sinner",
+#                  "Daniil Medvedev", "Stefanos Tsitsipas",
+#                  "Alexander Zverev", "Andrey Rublev", "Casper Ruud",
+#                  "Hubert Hurkacz", "Rafael Nadal"]
+
+
+
