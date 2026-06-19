153197e55bfd21fb7e2fb01d1676a4b2c7bd40c1
hssboston <hanna.s.suzuki@gmail.com>
2026-06-19 17:19:04 -0400

    Update multi-player-edge.py

--- code/multi-player-edge.py
+++ code/multi-player-edge.py
@@ -1,4 +1,8 @@
-import pandas as pd
+from dataloader import MCPDataLoader
+from winprob import computeV
+from eventweights import computeDeltaV, computeW
+from edge import computeEdge
+from pprint import pprint
 
 playersW = ["Iga Swiatek", "Aryna Sabalenka", "Coco Gauff",
                  "Elena Rybakina", "Jessica Pegula", "Mirra Andreeva",
@@ -8,6 +12,23 @@ playersW = ["Iga Swiatek", "Aryna Sabalenka", "Coco Gauff",
 #                  "Daniil Medvedev", "Stefanos Tsitsipas",
 #                  "Alexander Zverev", "Andrey Rublev", "Casper Ruud",
 #                  "Hubert Hurkacz", "Rafael Nadal"]
+
+dl = MCPDataLoader("w")
+points  = dl.points
+matches = dl.matches
+
+vDict, vDf, pointsV = computeV(points)
+vDfSorted = vDf.sort_values(["game win prob"])
+print(vDfSorted)
+
+pointsDeltaV = computeDeltaV(pointsV, vDict)
+wDict, wDf = computeW(pointsDeltaV)
+print(wDf)
+#     print( wDict )
+
+edge, summary = computeEdge("Aryna Sabalenka", pointsDeltaV, matches, wDict)
+print(edge)
+pprint(summary)
 
 
 
