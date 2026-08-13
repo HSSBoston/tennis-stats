9d9e344e334e6ac7f1a040b5ae886ae20424289b
hssboston <hanna.s.suzuki@gmail.com>
2026-08-13 17:58:57 -0400

    Create metrics-comparison.py

---
+++ code/otherstats/metrics-comparison.py
@@ -0,0 +1,168 @@
+import pathlib
+import sys
+PRJ_DIR = pathlib.Path(__file__).parents[1]  # 2 levels up
+sys.path.append(str(PRJ_DIR))
+
+import pandas as pd
+from dataloader import MCPDataLoader
+from edge import EdgeCalc
+from dr import DrCalc
+from drplus import DrPlusCalc
+from constants import OUTPUT_DIR
+
+# WTA top 100 players as of 05/25/2026
+players = [
+    "Aryna Sabalenka",
+    "Elena Rybakina",
+    "Iga Swiatek",
+    "Coco Gauff",
+    "Jessica Pegula",
+    "Amanda Anisimova",
+    "Elina Svitolina",
+    "Mirra Andreeva",
+    "Victoria Mboko",
+    "Karolina Muchova",
+    "Belinda Bencic",
+    "Linda Noskova",
+    "Jasmine Paolini",
+    "Ekaterina Alexandrova",
+    "Marta Kostyuk",
+    "Naomi Osaka",
+    "Iva Jovic",
+    "Sorana Cirstea",
+    "Madison Keys",
+    "Clara Tauson",
+    "Elise Mertens",
+    "Leylah Fernandez",
+    "Diana Shnaider",
+    "Anna Kalinskaya",
+    "Emma Navarro",
+    "Hailey Baptiste",
+    "Liudmila Samsonova",
+    "Marie Bouzkova",
+    "Ann Li",
+    "Anastasia Potapova",
+    "Jelena Ostapenko",
+    "Jaqueline Cristian",
+    "Cristina Bucsa",
+    "Xin Yu Wang",  # Xinyu Wang
+    "Sara Bejlek",
+    "Katerina Siniakova",
+    "Alexandra Eala",
+    "Elisabetta Cocciaretto",
+    "Emma Raducanu",
+    "Janice Tjen",
+    "Barbora Krejcikova",
+    "Tereza Valentova",
+    "Lois Boisson",
+    "Marketa Vondrousova",
+    "Dayana Yastremska",
+    "Magdalena Frech",
+    "Laura Siegemund",
+    "Mccartney Kessler",
+    "Maria Sakkari",
+    "Jessica Bouzas Maneiro",
+    "Petra Marcinko",
+    "Maya Joint",
+    "Daria Kasatkina",
+    "Tatjana Maria",
+    "Yuliia Starodubtseva",
+    "Qinwen Zheng",
+    "Anna Bondar",
+    "Talia Gibson",
+    "Panna Udvardy",
+    "Anhelina Kalinina",
+    "Shuai Zhang",
+    "Sonay Kartal",
+    "Caty Mcnally",
+    "Antonia Ruzic",
+    "Oleksandra Oliynykova",
+    "Zeynep Sonmez",
+    "Elsa Jacquemot",
+    "Solana Sierra",
+    "Nikola Bartunkova",
+    "Varvara Gracheva",
+    "Katie Boulter",
+    "Donna Vekic",
+    "Magda Linette",
+    "Renata Zarazua",
+    "Taylor Townsend",
+    "Yulia Putintseva",
+    "Elena Gabriela Ruse",  # Elena-Gabriela Ruse
+    "Peyton Stearns",
+    "Alycia Parks",
+    "Anastasia Zakharova",
+    "Eva Lys",
+    "Viktorija Golubic",
+    "Kimberly Birrell",
+    "Veronika Erjavec",
+    "Veronika Kudermetova",
+    "Camila Osorio",
+    "Sofia Kenin",
+    "Oksana Selekhmeteva",
+    "Kamilla Rakhimova",
+    "Lilli Tagger",
+    "Simona Waltert",
+    "Diane Parry",
+    "Daria Snigur",
+    "Emiliana Arango",
+    "Tamara Korpatsch",
+    "Ella Seidel",
+    "Lanlana Tararudee",
+    "Sinja Kraus",
+    "Hanne Vandewinkel",
+    "Ajla Tomljanovic"
+]
+
+dl = MCPDataLoader("w")
+
+rankDf = pd.DataFrame( {
+    "player":   players,
+    "WTA_rank": range(1, len(players) + 1)
+} )
+
+# Calculate EDGE
+
+_, edgeDf = EdgeCalc(dl.points, dl.matches).playersEdge(players)
+edgeDf = edgeDf[ ["player", "EDGE", "matches"] ].copy()
+edgeDf = edgeDf.rename( columns={"matches": "EDGE_matches"} )
+
+
+# Calculate DR
+
+_, drDf = DrCalc(dl.points, dl.matches).playersDr(players)
+drDf = drDf[ ["player", "DR", "matches"] ].copy()
+drDf = drDf.rename( columns={"matches": "DR_matches"} )
+
+# Calculate DR+
+
+_, drPlusDf = DrPlusCalc(dl.points, dl.matches).playersDrPlus(players)
+drPlusDf = drPlusDf[ ["player", "DR+", "matches"] ].copy()
+drPlusDf = drPlusDf.rename( columns={"matches": "DRPlus_matches"} )
+
+resultDf = (
+    rankDf
+    .merge(edgeDf,   on="player", how="left")
+    .merge(drDf,     on="player", how="left")
+    .merge(drPlusDf, on="player", how="left") )
+
+resultDf = resultDf[ [
+    "player",
+    "WTA_rank",
+    "EDGE",
+    "DR",
+    "DR+",
+    "EDGE_matches",
+    "DR_matches",
+    "DRPlus_matches" ]]
+
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None)
+
+print(resultDf)
+
+
+outputFile = OUTPUT_DIR / "edge-dr-drplus-wta-comparison.csv"
+resultDf.to_csv(outputFile, index=False)
+
+print(f"\nSaved to: {outputFile}")
